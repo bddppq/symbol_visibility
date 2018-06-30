e5566249507dbb2f7c5eb7697033a84d068d7340
Junjie Bai <bai@in.tum.de>
2018-06-30 02:10:22 -0700

    .

--- foo.h
+++ foo.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #if defined (_WIN32)
-#if defined (foo_EXPORTS) || defined (another_bar_EXPORTS)
-#define  MYLIB_EXPORT __declspec(dllexport)
-#else /* defined (foo_EXPORTS) || defined (another_bar_EXPORTS) */
-#define  MYLIB_EXPORT __declspec(dllimport)
-#endif /* defined (foo_EXPORTS) || defined (another_bar_EXPORTS) */
+  #if defined (foo_EXPORTS) || defined (another_bar_EXPORTS)
+    #define  MYLIB_EXPORT __declspec(dllexport)
+  #else /* defined (foo_EXPORTS) || defined (another_bar_EXPORTS) */
+    #define  MYLIB_EXPORT __declspec(dllimport)
+  #endif /* defined (foo_EXPORTS) || defined (another_bar_EXPORTS) */
 #else /* defined (_WIN32) */
- #define MYLIB_EXPORT
+  #define MYLIB_EXPORT
 #endif /* defined (_WIN32) */
 
 namespace my {
