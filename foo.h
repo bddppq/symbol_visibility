e697f1ec3938decbed2bf72b6c37b5e934c6247b
Junjie Bai <bai@in.tum.de>
2018-06-30 00:57:26 -0700

    .

--- foo.h
+++ foo.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#if defined (_WIN32)
+  #if defined(MyLibrary_EXPORTS)
+    #define  MYLIB_EXPORT __declspec(dllexport)
+  #else
+    #define  MYLIB_EXPORT __declspec(dllimport)
+  #endif /* MyLibrary_EXPORTS */
+#else /* defined (_WIN32) */
+ #define MYLIB_EXPORT
+#endif
+
 namespace my {
-void Foo();
-void Bar();
+void MYLIB_EXPORT Foo();
+void MYLIB_EXPORT Bar();
 }
